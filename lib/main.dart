ae86e7e15c91e517a7fba45ea98b855ed31af564
romel diaz <root.romel@gmail.com>
2026-06-16 23:48:34 -0500

    menu

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,61 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+
+}
+
+class _MainAppState extends State<MainApp> {
+  int _selectedIndex = 0;
+
+
+  final List<Widget> _pages = const [
+    Center(child: Text('Inicio')),
+    Center(child: Text('Buscar')),
+    Center(child: Text('Perfil')),
+  ];
+  
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: _pages[_selectedIndex],
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _selectedIndex,
+          onDestinationSelected: _onItemTapped,
+          destinations: const [
+            NavigationDestination( 
+              icon: Icon(Icons.home_outlined),
+              selectedIcon: Icon(Icons.home),
+              label: 'Inicio'
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.search_outlined),
+              selectedIcon: Icon(Icons.search),
+              label: 'Buscar'
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.person_outline),
+              selectedIcon: Icon(Icons.person),
+              label: 'Perfil'
+            )
+          ]
         ),
-      ),
+      )
     );
   }
 }
